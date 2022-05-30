158fb3962acd9a9f9018d749dd3ee401bd29aa43
kiriti <kiriti.k999@gmail.com>
2022-05-30 23:53:14 +0530

    path test

--- short_funnel/app.py
+++ short_funnel/app.py
@@ -1,11 +1,11 @@
 import json
 import requests
 def lambda_handler(event, context):
-    print(event)
+    print('Backend event:: ', event)
     try:
         ip = requests.get("http://checkip.amazonaws.com/")
     except requests.RequestException as e:
-        print(e)
+        print('error in backend', e)
         raise e
     return {
         "statusCode": 200,
