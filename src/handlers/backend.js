3c0c5d6971b3f4e0c929e2e7d2fcaf819ac3c95a
kiriti <kiriti.k999@gmail.com>
2022-06-01 22:42:18 +0530

    test

--- src/handlers/backend.js
+++ src/handlers/backend.js
@@ -1,19 +1,25 @@
 const axios = require('axios');
+var http = require('http');
 
 exports.lambda_handler = async (event) => {
-    console.log('lambda_handler:: event: ', event);
+    console.log('backend:: lambda_handler:: event: ', event);
     let ip;
     try {
         ip = await axios.get('http://checkip.amazonaws.com/');
+        console.log('backend:: lambda_handler:: ip:', ip);
     } catch (error) {
-        console.error('error', error);
-        throw new Error('Backend error...');
+        console.error('backend:: lambda_handler::', error);
     }
+
     return {
-        "statusCode": 200,
-        "body": {
-            "message": "Hello from Lambda backend.",
-            "yourIpAddress": ip
-        }
-    };
+        statusCode: 200,
+        body: JSON.stringify(ip)
+    }
+    // return {
+    //     "statusCode": 200,
+    //     "body": {
+    //         "message": "Hello from Lambda backend.",
+    //         "yourIpAddress": ip
+    //     }
+    // };
 };
